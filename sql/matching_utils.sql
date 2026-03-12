5567fd189b079f84bb7fe388176d321fe136c32a
David M Walker <davidw@datamgmt.com>
2026-03-12 12:29:05 +0000

    Updates

--- sql/matching_utils.sql
+++ sql/matching_utils.sql
@@ -41,25 +41,53 @@ WHERE
     AND match_score IS null
 ORDER BY id;
 
+SELECT
+    id,
+    station,
+    artist,
+    title,
+    matched_source
+    FROM matches AS m
+WHERE
+    NOT EXISTS (
+        SELECT 1
+        FROM matched AS d
+        WHERE
+            d.artist = m.artist
+            AND d.title = m.title
+    )
+    AND station IN ('absolute80s','smooth80s','heart80s','absolute70s')
+    AND match_score IS NULL
+    AND matched_artist IS NULL
+    AND matched_title IS NULL
+    AND match_score is NULL
+ORDER BY id, station, artist, title,matched_source;
+
+
 UPDATE matches SET matched_source = 'wantlist'
 WHERE
-    station IN ('absolute80s','smooth80s','heart80s')
-    AND id IN (###)
-    AND id <25000;
+    station IN ('absolute80s','smooth80s','heart80s','absolute70s')
+    AND id IN (1501,2136,2283,2444,2456,2638,3067,3071,3073,3389,3392,3753,4535,4537,5018,5019,5562,5563,6238,6243,6244)
+    AND id <7000;
 
 UPDATE matches
 SET matched_source = 'no single'
 WHERE
-    station IN ('absolute80s','smooth80s','heart80s')
-    AND id IN (###)
-    AND id <25000;
+    station IN ('absolute80s','smooth80s','heart80s','absolute70s')
+    AND id IN (4136,6239)
+    AND id <7000;
 
 UPDATE matches
 SET matched_source = 'collection'
 WHERE
-    station IN ('absolute80s','smooth80s','heart80s')
+    station IN ('absolute80s','smooth80s','heart80s','absolute70s')
     AND matched_source IS null
-    AND id <25000;
+    AND id <7000;
+
+ select station, matched_source, count(*) 
+ from matches 
+ where station IN ('absolute80s','smooth80s','heart80s','absolute70s') 
+ group by station, matched_source;
 
 SELECT
     id,

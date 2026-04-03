c5e33f301480ca6ddc7accbfcf8fe2bc988332f0
David M Walker <davidw@datamgmt.com>
2026-04-03 12:10:57 +0100

    Add scripts

---
+++ sql/match_check.sql
@@ -0,0 +1,21 @@
+SELECT
+    id,
+    station,
+    artist,
+    title,
+    matched_source
+FROM matches AS m
+WHERE
+    NOT EXISTS (
+        SELECT 1
+        FROM matched AS d
+        WHERE
+            d.artist = m.artist
+            AND d.title = m.title
+    )
+    AND station IN (SELECT station FROM stations WHERE validated = 'Y')
+    AND match_score IS null
+    AND matched_artist IS null
+    AND matched_title IS null
+    AND match_score IS null
+ORDER BY id, station, artist, title, matched_source;

ac77e32f7034ad4bdbfa631a56549758dbf18959
David M Walker <davidw@datamgmt.com>
2026-04-12 16:29:22 +0100

    Updates

--- sql/match_check.sql
+++ sql/match_check.sql
@@ -19,3 +19,29 @@ WHERE
     AND matched_title IS null
     AND match_score IS null
 ORDER BY id, station, artist, title, matched_source;
+
+SELECT
+    country,
+    station,
+    matched_source,
+    COUNT(play_count) AS title_count,
+    CAST(
+        ROUND(
+            100.0 * COUNT(play_count)
+            / SUM(COUNT(play_count)) OVER(PARTITION BY station),
+            1
+        ) AS REAL
+    ) AS title_count_percentage,
+    SUM(play_count) AS play_count,
+    CAST(
+        ROUND(
+            100.0 * SUM(play_count)
+            / SUM(SUM(play_count)) OVER(PARTITION BY station),
+            1
+        ) AS REAL
+    ) AS play_count_percentage
+FROM v_analysis
+GROUP BY
+    country,
+    station,
+    matched_source;

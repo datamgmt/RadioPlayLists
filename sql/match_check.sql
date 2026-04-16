9463b5533f91b4dd6c7ac940d8f0d67becc17510
David M Walker <davidw@datamgmt.com>
2026-04-16 09:15:21 +0100

    Update match_check.sql

--- sql/match_check.sql
+++ sql/match_check.sql
@@ -52,7 +52,9 @@ SELECT
     sum(m.matched_source = 'wantlist') AS wantlist,
     sum(m.matched_source = 'no vinyl') AS no_vinyl,
     sum(1) AS total,
-    100 * sum(m.matched_source = 'collection') / sum(1) AS pct_collection
+    100 * sum(m.matched_source = 'collection') / sum(1) AS pct_collection,
+    100 * sum(m.matched_source = 'wantlist') / sum(1) AS pct_wantlist,
+    100 * sum(m.matched_source = 'no vinyl') / sum(1) AS pct_no_vinyl
 FROM v_playlists AS v,
     matched AS m
 WHERE

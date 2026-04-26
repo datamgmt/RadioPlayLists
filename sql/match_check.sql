e3e13f8e00c1061e6205d3d9fc9e5da3e50f1fc2
David M Walker <davidw@datamgmt.com>
2026-04-26 14:45:55 +0100

    Update match_check.sql

--- sql/match_check.sql
+++ sql/match_check.sql
@@ -1,78 +1,65 @@
 SELECT
-    id,
-    station,
-    artist,
-    title,
-    matched_source
-FROM matches AS m
-WHERE
-    NOT EXISTS (
-        SELECT 1
-        FROM matched AS d
-        WHERE
-            d.artist = m.artist
-            AND d.title = m.title
-    )
-    AND station IN (SELECT station FROM stations WHERE validated = 'Y')
-    AND match_score IS null
-    AND matched_artist IS null
-    AND matched_title IS null
-    AND match_score IS null
-ORDER BY id, station, artist, title, matched_source;
-
-SELECT
     country,
     station,
     matched_source,
-    COUNT(play_count) AS title_count,
-    ROUND(
+
+    printf('%11d', COUNT(play_count)) AS title_count,
+
+    printf('%22.2f',
         100.0 * COUNT(play_count)
-        / SUM(COUNT(play_count)) OVER(PARTITION BY station), 2
+        / SUM(COUNT(play_count)) OVER(PARTITION BY station)
     ) AS title_count_percentage,
-    SUM(play_count) AS play_count,
-    ROUND(
+
+    printf('%10d', SUM(play_count)) AS play_count,
+
+    printf('%21.2f',
         100.0 * SUM(play_count)
-        / SUM(SUM(play_count)) OVER(PARTITION BY station), 2
+        / SUM(SUM(play_count)) OVER(PARTITION BY station)
     ) AS play_count_percentage
 FROM v_analysis
 GROUP BY
     country,
     station,
     matched_source;
+    
 
 SELECT
     date(v.play_datetime) AS date,
-    sum(m.matched_source = 'collection') AS collection,
-    sum(m.matched_source = 'wantlist') AS wantlist,
-    sum(m.matched_source = 'no vinyl') AS no_vinyl,
-    sum(1) AS total,
-    ROUND(
-        100.0 * sum(m.matched_source = 'collection') / sum(1), 2 
-    )AS pct_collection,
-    ROUND(
-        100.0 * sum(m.matched_source = 'wantlist') / sum(1), 2 
+
+    printf('%10d', sum(m.matched_source = 'collection')) AS collection,
+    printf('%8d', sum(m.matched_source = 'wantlist')) AS wantlist,
+    printf('%8d', sum(m.matched_source = 'no vinyl')) AS no_vinyl,
+    printf('%5d', sum(1)) AS total,
+
+    printf('%14.2f',
+        100.0 * sum(m.matched_source = 'collection') / sum(1)
+    ) AS pct_collection,
+
+    printf('%12.2f',
+        100.0 * sum(m.matched_source = 'wantlist') / sum(1)
     ) AS pct_wantlist,
-    ROUND(
-        100.0 * sum(m.matched_source = 'no vinyl') / sum(1), 2 
+
+    printf('%12.2f',
+        100.0 * sum(m.matched_source = 'no vinyl') / sum(1)
     ) AS pct_no_vinyl
-FROM v_playlists AS v,
-    matched AS m
+FROM v_playlists AS v
+JOIN matched AS m
+    ON v.artist = m.artist
+   AND v.title = m.title
 WHERE
     v.country = 'uk'
     AND v.station = 'absolute80s'
-    AND v.artist = m.artist
-    AND v.title = m.title
-    AND play_datetime >= date('now', '-7 days')
+    AND v.play_datetime >= date('now', '-7 days')
 GROUP BY
     date(v.play_datetime)
 ORDER BY
     date(v.play_datetime);
-
+    
 WITH daily AS (
     SELECT
         date(v.play_datetime) AS date,
-        ROUND(
-            100.0 * sum(m.matched_source = 'collection') / sum(1), 2
+        printf('%14.2f',
+            100.0 * sum(m.matched_source = 'collection') / sum(1)
         ) AS pct_collection
     FROM v_playlists AS v
     JOIN matched AS m
@@ -83,7 +70,9 @@ WITH daily AS (
         AND v.station = 'absolute80s'
     GROUP BY date(v.play_datetime)
 )
-SELECT *
+SELECT
+    date,
+    printf('%14.2f', pct_collection) AS pct_collection
 FROM daily
 WHERE pct_collection = (SELECT MAX(pct_collection) FROM daily)
-ORDER BY date;
+ORDER BY date;    

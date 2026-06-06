f35d0854b96226d12125a4efb657e15087953fc3
David M Walker <davidw@datamgmt.com>
2026-06-06 15:30:50 +0100

    Updates

--- sql/create_boxes.sql
+++ sql/create_boxes.sql
@@ -6,5 +6,15 @@ SELECT *
 FROM v_record_cases;
 
 .output
+
+.output data/box_counts.csv
+
+select  collection_folder, 
+        count(*) record_count 
+from    collection 
+group by collection_folder;
+
+.output
+
 .mode column
 .echo on

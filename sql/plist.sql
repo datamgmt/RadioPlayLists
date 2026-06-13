5f22b89346602611bd880d6a0fe82d4b056f5faf
David M Walker <davidw@datamgmt.com>
2026-06-13 13:58:00 +0100

    Update plist.sql

--- sql/plist.sql
+++ sql/plist.sql
@@ -7,57 +7,57 @@ CREATE TABLE IF NOT EXISTS priority_list
      play_date
     );
 
-DROP VIEW IF EXISTS v_absolute80s_wantlist;
-
-CREATE VIEW IF NOT EXISTS v_absolute80s_wantlist
-AS
-SELECT
-    artist,
-    title,
-    date(play_datetime) AS play_date
-FROM v_playlists
-WHERE
-    country = 'uk'
-    AND station = 'absolute80s'
-    AND matched_source = 'wantlist'
-    and artist||title not in (select artist||title from priority_list);
-
-DROP VIEW IF EXISTS v_min_playlist_wantdate;
-
-CREATE VIEW IF NOT EXISTS v_min_playlist_wantdate
-AS
-SELECT
-    play_date,
-    count(*) AS play_count
-FROM v_absolute80s_wantlist
-WHERE play_date NOT IN (SELECT play_date FROM priority_list)
-GROUP BY play_date ORDER BY play_count, play_date LIMIT 1;
-
 INSERT INTO priority_list (artist, title, play_date)
 SELECT
     a.artist,
     a.title,
     a.play_date
-FROM v_absolute80s_wantlist a
+FROM (
+    SELECT
+        artist,
+        title,
+        date(play_datetime) AS play_date
+    FROM v_playlists
+    WHERE
+        country = 'uk'
+        AND station = 'absolute80s'
+        AND matched_source = 'wantlist'
+        AND artist || title NOT IN (SELECT artist || title FROM priority_list)
+) AS a
 WHERE
-    a.play_date IN (SELECT m.play_date FROM v_min_playlist_wantdate m)
-    AND a.artist || a.title NOT IN (SELECT p.artist || p.title FROM priority_list p)
-ORDER BY artist, title
-;
+    a.play_date IN (SELECT m.play_date FROM (SELECT
+        play_date,
+        count(*) AS play_count
+    FROM (
+        SELECT
+            artist,
+            title,
+            date(play_datetime) AS play_date
+        FROM v_playlists
+        WHERE
+            country = 'uk'
+            AND station = 'absolute80s'
+            AND matched_source = 'wantlist'
+            AND artist
+            || title NOT IN (SELECT artist || title FROM priority_list
+            )
+    )
+    WHERE play_date NOT IN (SELECT play_date FROM priority_list)
+    GROUP BY play_date ORDER BY play_count, play_date LIMIT 1) AS m)
+    AND a.artist
+    || a.title NOT IN (SELECT p.artist || p.title FROM priority_list AS p)
+ORDER BY a.artist, a.title;
 
-SELECT  id as "ID",
-        artist as "Artist",
-        title as "Title",
-        play_date AS "Date"
-FROM priority_list;
-
-select 
-CASE 
-    WHEN artist LIKE 'THE %' THEN 
-        REPLACE(SUBSTR(artist, 5), ' and ', ' & ')
-    ELSE
-        REPLACE(artist, ' and ', ' & ')
-END AS "Artist",
-        title as "Title"
+SELECT
+    id,
+    CASE
+        WHEN artist LIKE 'THE %'
+            THEN
+                replace(substr(artist, 5), ' and ', ' & ')
+        ELSE
+            replace(artist, ' and ', ' & ')
+    END AS artist,
+    title,
+    play_date
 FROM priority_list
-order by id;
+ORDER BY id;
